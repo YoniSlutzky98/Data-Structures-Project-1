4aca2a61016af7ab3978d417c99613ce8ae0a463
guyb1 <guyb181@gmail.com>
2021-11-25 18:41:04 +0200

    main added for tests

--- src/AVLTree.java
+++ src/AVLTree.java
@@ -921,5 +921,56 @@ public class AVLTree {
 		}
   }
 
+	static void print_mat(String[][] mat) {
+		int height = mat.length;
+		int width = mat[0].length;
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
+				if (mat[i][j] != null) {
+					System.out.print(mat[i][j]);
+				} else {
+					System.out.print(" ");
+				}
+				//System.out.print(mat[i][j] + " ");
+			}
+			System.out.println();
+
+		}
+	}
+
+	static void fill_mat(AVLTree.IAVLNode root, String[][] mat, int i, int j) {
+		if (!root.isRealNode()) {
+			return;
+		}
+		int height = root.getHeight() + 1;
+		int width = 6 * (int) Math.pow(2, height);
+		mat[i][j + width / 2] = Integer.toString(root.getKey());
+		fill_mat(root.getLeft(), mat, i + 2, j);
+		fill_mat(root.getRight(), mat, i + 2, j - 1 + width / 2);
+	}
+
+	static void print_tree(AVLTree tree) {
+		if (tree.empty()) {
+			System.out.println("empty tree lol");
+			return;
+		}
+		AVLTree.IAVLNode root = tree.getRoot();
+		int height = root.getHeight() + 1;
+		int width = 6 * (int) Math.pow(2, height);
+		String[][] mat = new String[2 * height][width];
+		fill_mat(root, mat, 0, 0);
+		print_mat(mat);
+
+	}
+
+  public static void main(String [] args) {
+	  AVLTree myTree = new AVLTree();
+	  myTree.insert(1, "hello");
+	  myTree.insert(2, "hello");
+	  myTree.insert(7, "hello");
+	  print_tree(myTree);
+
+  }
+
 }
   
